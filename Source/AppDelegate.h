2b358da1bd1dcd41977ed0a31ea05968e9458dc4
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-05-13 11:36:06 -0400

    Add files

---
+++ Source/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Elephanterpreter
+//
+//  Created by Jeff Kereakoglow on 5/13/16.
+//  Copyright © 2016 Alexis Digital. All rights reserved.
+//
+
+@import UIKit;
+
+@interface AppDelegate: UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Source/ViewController.m
@@ -0,0 +1,15 @@
+//
+//  ViewController.m
+//  Elephanterpreter
+//
+//  Created by Jeff Kereakoglow on 5/13/16.
+//  Copyright © 2016 Alexis Digital. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@end
+
+@implementation ViewController
+@end
